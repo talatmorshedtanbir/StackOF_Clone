498bb76178f4830c9cfceb08d57bd77a89ec6860
hossaintanbir0 <hossaintanbir0@gmail.com>
2020-12-12 03:13:25 +0600

    Admin area is working now

---
+++ StackOF_Clone.Web/StackOF_Clone/Areas/Admin/Controllers/MemberAccountController.cs
@@ -0,0 +1,29 @@
+﻿using StackOF_Clone.Web.Areas.Admin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackOF_Clone.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class MemberAccountController : Controller
+    {
+        // GET: Admin/MemberAccount
+        public ActionResult Index()
+        {
+            var model = new MemberViewModel();
+
+            return View(model);
+        }
+
+        public ActionResult GetMembers()
+        {
+            var model = new MemberViewModel();
+            var data =model.GetMemberList();
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ StackOF_Clone.Web/StackOF_Clone/Areas/Admin/Models/MemberViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using NHibernate.AspNet.Identity;
+using StackOF_Clone.Core.Database.Contexts;
+using StackOF_Clone.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackOF_Clone.Web.Areas.Admin.Models
+{
+    public class MemberViewModel
+    {
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        internal object GetMemberList()
+        {
+            var session = FNHibernateContext.SessionOpen();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(session));
+
+            var users = userManager.Users.ToList();
+
+            return new
+            {
+                 data = (from user in users
+                 select new string[]
+                 {
+                           user.Email,
+                           user.PhoneNumber,
+                           user.Id.ToString()
+                 }
+                 ).ToArray()
+            };
+        }
+    }
+}
