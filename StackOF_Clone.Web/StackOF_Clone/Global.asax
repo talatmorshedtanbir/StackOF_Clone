4ef681c607fc18ed1f258b7775d4e0e19716d033
hossaintanbir0 <hossaintanbir0@gmail.com>
2020-12-11 23:14:00 +0600

    added initial project

---
+++ StackOF_Clone.Web/StackOF_Clone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StackOF_Clone.MvcApplication" Language="C#" %>
---
+++ StackOF_Clone.Web/StackOF_Clone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StackOF_Clone.Startup))]
+namespace StackOF_Clone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
